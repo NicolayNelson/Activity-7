82e99094d396b85e331b4dcc44f13e31242984ec
NicolayNelson <59945806+NicolayNelson@users.noreply.github.com>
2020-02-29 19:25:15 -0800

    Finished README file and uploaded attempted at #2
    Did not complete #2 as i did not know how the HTML file worked.

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Activity-7
+
+Made a balloon inflate and deflate with the up and down key
+Tried to do the second question but after hours of watching videos and not getting it ran out of time.
+(Did not understand the HTML and where files should go)
--- script.js
+++ script.js
@@ -27,6 +27,7 @@ window.addEventListener("keydown", event => {
   }
 });
 
+
 // 2. The index.html page has a tabbed layout. Make the default state of the layout show
 // the first tab, and make it so that when you click the links at the top the correct
 // tab's contents are displayed and the others are hidden. Prevent the default action of
@@ -34,3 +35,22 @@ window.addEventListener("keydown", event => {
 // function as expected. There are many ways to accomplish this task, but you will need
 // to at minimum add listeners to each link and toggle the display of the tab contents.
 // Hint: display: none; hides an element, and display: block; will bring it.
+
+
+// const tabs = document.getElementById("tabbed-layout");
+// const panels = document.getElementsByTagName("tabbed-contents");
+// tabs.addEventListener('click'.function(e));{
+//   if(e.target.tagName == "LI"){
+// const targetPanel = document.getElementById("tab1Link");
+// panels.forEach(function(panel) {
+//   if (panel == targetPanel){
+//     panel.classList.add('active');
+//   } else {
+//     panel.classList.remove('active');
+//   }
+// })
+
+//I tried for hours and just cant understand this i watched video after video each saying a different thing
+// I do not understand the HTML and while i did not have to write any HTML i did not understand how to ead it.
+// I put my last attempt up before submitting because i will have ran out of time and i would like something
+//instead of a zero on the grade.
